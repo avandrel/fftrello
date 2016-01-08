2afb7906d390bec096ced6928e0154c0d4e03e05
Avandrel <m.choroszy@mgpm.pl>
2016-01-08 23:29:08 +0100

    Add sent current page

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var self = require('sdk/self');
 var contextMenu = require("sdk/context-menu");
 var Request = require("sdk/request").Request;
 
-var token = "p0huRzD8YD3wyRtW7WDclNuR";
+var token = "02T80GqJBJ5jdFVrwBXWDRI2";
 var url = "http://ditrello.mgpm.pl/create";
+//var url = "http://localhost:9292/create";
 
 function show_response(responseText) {
 	console.log(responseText)
@@ -12,19 +13,30 @@ function show_response(responseText) {
 function post(text) {
   Request({
       url: url,
-      content: "token="+token+"&text=Zgłaszam link"+text+"&trigger_word=Zgłaszam link",
+      content: "token="+token+'&text="'+text+'"',
       onComplete: function (response) {
         show_response(response.text);
       }
     }).post();
 }
 
-var menuItem = contextMenu.Item({
-  label: "Send to Trello",
+var menuItemURL = contextMenu.Item({
+  label: "Send selected URL to Trello",
   context: [contextMenu.URLContext("*"), contextMenu.SelectorContext("a[href], img")],
   contentScript: 'self.on("click", function (node, data) {' +
     			 '  self.postMessage(node.href||node.src);' +
     			 '});',
+  onMessage: function (selectionText) {
+    post(selectionText)
+  }
+});
+
+var menuItemPage = contextMenu.Item({
+  label: "Send page URL to Trello",
+  context: contextMenu.PageContext(),
+  contentScript: 'self.on("click", function (node, data) {' +
+           '  self.postMessage(window.top.location.href);' +
+           '});',
   onMessage: function (selectionText) {
     post(selectionText)
   }
